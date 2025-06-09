82fefcc183179a5fbb41c4612a3147f25f2e0424
Kevin <124320719+Kevincin02@users.noreply.github.com>
2025-06-08 21:25:28 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tarea2.Models;
+
+namespace Tarea2.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new TrianguloModel());
+        }
+
+
+        //Todas las validaciones y resultados
+        [HttpPost]
+        public ActionResult Index(TrianguloModel trianguloModel)
+        {
+            //Validacion de 0s, con el annotation range
+            if (!ModelState.IsValid)
+            {
+                return View(trianguloModel);
+            }
+            //Validacion de desigualdad triangular
+            if (trianguloModel.a >= trianguloModel.b + trianguloModel.c || 
+                trianguloModel.b >= trianguloModel.a + trianguloModel.c ||
+                trianguloModel.c >= trianguloModel.a + trianguloModel.b)
+            {
+                ModelState.AddModelError("", "Los valores no cumplen con la desigualdad triangular");
+                return View(trianguloModel);
+            }
+            //Calculo del Perimetro
+            ViewBag.Perimetro = trianguloModel.CalcularPerimetro();
+            //Calculo del SemiPerimetro
+            ViewBag.SemiPerimetro = trianguloModel.CalcularSemiPerimetro();
+            //Calculo del Area
+            ViewBag.Area = trianguloModel.CalcularArea();
+            //Se determina el tipo de triangulo que es
+            ViewBag.Tipo = trianguloModel.TipoTriangulo();
+            //Se calcula el valor de alfa
+            ViewBag.Alfa = trianguloModel.CalcularAlfa();
+            //Se calcula el valor de beta
+            ViewBag.Beta = trianguloModel.CalcularBeta();
+            //Se calcula el valor de gamma
+            ViewBag.Gamma = trianguloModel.CalcularGamma();
+
+
+
+
+
+            return View(trianguloModel);
+        }
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+    }
+}
---
+++ Models/TrianguloModel.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+
+namespace Tarea2.Models
+{
+    public class TrianguloModel
+    {
+        [Range(1, 100, ErrorMessage = "El numero no puede ser igual a 0")]
+        public double a { get; set; }
+
+        [Range(1, 100, ErrorMessage = "El numero no puede ser igual a 0")]
+        public double b { get; set; }
+
+        [Range(1, 100, ErrorMessage = "El numero no puede ser igual a 0")]
+        public double c { get; set; }
+
+
+        public double P { get; set; }
+        public double S { get; set; }
+        public double Area { get; set; }
+
+        //Valores para calulo de angulos
+        public double ValorA { get; set; }
+        public double ValorB { get; set; }
+        public double ValorC { get; set; }
+        public double CosA { get; set; }
+        public double CosB { get; set; }
+        public double CosC { get; set; }
+
+
+
+        //Calculo del perimetro
+        public double CalcularPerimetro()
+        {
+            P = a + b + c;
+            return P;
+        }
+        //Calculo del semiperimetro
+        public double CalcularSemiPerimetro()
+        {
+            double S = (a+b+c)/2.0;
+            return S;
+        }
+        //Calculo del Area
+        public double CalcularArea()
+        {
+            double S = CalcularSemiPerimetro();
+            return Math.Sqrt(S * (S - a) * (S - b) * (S - c));
+        }
+        //Se determina que tipo de triangulo es #Equilatero, Isosceles o Escaleno
+        public String TipoTriangulo()
+        {
+            if(a==b && b==c)
+            {
+                return "Equilatero";
+            }
+            else if (a == b || a == c || b == c)
+            {
+                return "Isosceles";
+            }
+            else
+            {
+                return "Escaleno";
+            }
+        }
+        //Calculo de angulo alfa
+        public double CalcularAlfa()
+        {
+            ValorA = Math.Pow(a, 2);
+            ValorB = Math.Pow(b, 2);
+            ValorC = Math.Pow(c, 2);
+
+            CosA= (ValorB + ValorC - ValorA) / (2 * b * c);
+
+            double anguloRadianes = Math.Acos(CosA);
+            double anguloGrados = anguloRadianes * (180 / Math.PI);
+            return anguloGrados;
+        }
+        //Calculo de angulo beta
+        public double CalcularBeta()
+        {
+            ValorA = Math.Pow(a, 2);
+            ValorB = Math.Pow(b, 2);
+            ValorC = Math.Pow(c, 2);
+
+            CosB = (ValorA + ValorC - ValorB) / (2 * a * c);
+
+            double anguloRadianes = Math.Acos(CosB);
+            double anguloGrados = anguloRadianes * (180 / Math.PI);
+            return anguloGrados;
+        }
+        //Calculo de angulo gamma
+        public double CalcularGamma()
+        {
+            ValorA = Math.Pow(a, 2);
+            ValorB = Math.Pow(b, 2);
+            ValorC = Math.Pow(c, 2);
+
+            CosC = (ValorA + ValorB - ValorC) / (2 * a * b);
+
+            double anguloRadianes = Math.Acos(CosC);
+            double anguloGrados = anguloRadianes * (180 / Math.PI);
+            return anguloGrados;
+        }
+
+    }
+}
